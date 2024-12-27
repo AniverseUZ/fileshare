3c3e021f0e5d68c3c6c13e886649b525a4109d23
Lovetheticx <140687771+lovetheticx@users.noreply.github.com>
2024-12-27 12:28:54 +0500

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6216046291"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Aniverse:assalom13@cluster0.ihmqfou.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Aniverse:assalom13@cluster0.aktuglw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "aniverse")
 
 #force sub channel id, if you want enable force sub
